d0d6d117ca12f96bf7e7238c3446be335340bb83
Max Malynowsky <github@maxmalynowsky.com>
2024-09-30 08:30:54 +0200

    add scores for columns

--- src/scores/__main__.py
+++ src/scores/__main__.py
@@ -23,7 +23,7 @@ def main() -> None:
     logger.info("Starting")
 
     # NOTE: Register scores here.
-    score_functions = (geometry, languages, dates, columns)
+    score_functions = (geometry, columns, languages, dates)
 
     metadata = read_csv(tables_dir / "metadata.csv")
     checks = read_csv(tables_dir / "checks.csv")
